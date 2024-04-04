7651c8abbdf6085c18b99594fa4ca0f13f527083
mike connor <mike@mikeconnor.tech>
2024-04-03 20:07:31 -0500

    tweak

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,65 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
 import eslintConfigPrettier from 'eslint-config-prettier';
-import globals from "globals";
+import globals from 'globals';
 import js from '@eslint/js';
 import markdown from 'eslint-plugin-markdown';
+import tsParser from '@typescript-eslint/parser';
+
+// import tsPlugin from '@typescript-eslint/eslint-plugin';
 
 export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   js.configs.recommended,
   ...eslintPluginAstro.configs['flat/recommended'],
+
   {
     languageOptions: {
-            ecmaVersion: 2022,
-            sourceType: "module",
-            globals: {
-                ...globals.browser,
-                // myCustomGlobal: "readonly"
-            }
-        }
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+
+        // alert: 'readonly',
+      },
+    },
   },
   {
+    files: ['**/*.ts', '**/*.tsx'],
+
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        ...globals.browser,
+        // myCustomGlobal: "readonly"
+      },
+      // sourceType: string;
+      parserOptions: {
+        ecmaVersion: 'latest',
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      // "prettier/prettier"?: undefined;
+    },
+    // processor: string;
+    // plugins?: undefined;
+  },
+
+  {
+    files: ['**/*.astro'],
+
+    // Parse the script in `.astro` as TypeScript by adding the following configuration.
+    // It's the setting you need when using TypeScript.
+
     rules: {
       // override/add rules settings here, such as:
       // "astro/no-set-html-directive": "error"
+      'no-unused-vars': 'warn',
+      // semi: 'warn',  this rule gets tossed out by eslintConfigPrettier
     },
   },
-  eslintConfigPrettier,
+
   {
     plugins: {
       markdown,
@@ -48,4 +82,5 @@ export default [
       // 'import/no-unresolved': 'off',
     },
   },
+  eslintConfigPrettier, // eslint-config-prettier last
 ];
